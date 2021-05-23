b8b0baa5d01820aaf1354195c09a62a78cd78935
michiru_matsuda <45786439+michirumatsuda@users.noreply.github.com>
2021-05-23 22:34:49 +0900

    Add user microposts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,8 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  # アップロードされたファイルをAWSに保存する
+  config.active_storage.service = :amazon
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
